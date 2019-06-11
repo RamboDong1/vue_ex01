11d8972da69feb6462b9bea5e7778be5071e1265
RamboDong1 <1119745549@qq.com>
2019-06-11 15:30:06 +0800

    import ElementUI from 'element-ui'

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 在main.js文件中引入ElementUI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 样式文件得单独引用进来， reset.css 是样式重置文件，根据自己项目情况引入
+// import 'element-ui/lib/theme-chalk/reset.css'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
