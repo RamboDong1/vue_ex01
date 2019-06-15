2cf4ea0a306a873510d07a4ea3e5113961f838ab
RamboDong1 <1119745549@qq.com>
2019-06-15 15:59:59 +0800

    20190615 16:00

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,39 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <el-container>
+    <el-header height="100px">
+      <h3>我的网站</h3>
+      <P>这里是网站头部</P>
+    </el-header>
+    <el-main>
+      <router-view/>
+    </el-main>
+    <el-footer>
+      这里是网站的底部
+    </el-footer>
+  </el-container>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<style>
+  .el-header{
+    background-color: dodgerblue;
+    color: white;
+    text-align: left;
   }
-}
+  .el-footer {
+    background-color: dodgerblue;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+    min-height: 100%;
+  }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-  Login
-  </div>
+  <el-row type="flex" justify="center">
+    <el-form label-width="80px">
+      <el-form-item label="用户名">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">登录</el-button>
+        <el-button >注册</el-button>
+      </el-form-item>
+    </el-form>
+  </el-row>
 </template>
 
 <script>
