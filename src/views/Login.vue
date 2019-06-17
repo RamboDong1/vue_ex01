436bb2bd4b803ef9d2777880549d08f4387faedf
RamboDong1 <1119745549@qq.com>
2019-06-17 20:24:53 +0800

    20190617 20:24

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,112 @@
 <template>
   <el-row type="flex" justify="center">
-    <el-form label-width="80px">
-      <el-form-item label="用户名">
-        <el-input></el-input>
+<!--    登录页面-->
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm" v-if="isLogin">
+      <el-form-item label="用户名" prop="name">
+        <el-input v-model="ruleForm.name" autocomplete="off"></el-input>
       </el-form-item>
-      <el-form-item label="密码">
-        <el-input type="password"></el-input>
+      <el-form-item label="密码" prop="pass">
+        <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">登录</el-button>
-        <el-button >注册</el-button>
+        <el-button type="primary" @click="login()">登录</el-button>
+        <el-button @click="reg()">注册</el-button>
+      </el-form-item>
+    </el-form>
+<!--    注册页面-->
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm" v-else>
+      <el-form-item label="用户名" prop="name">
+        <el-input v-model="ruleForm.name" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="pass">
+        <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+        <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">确定</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
       </el-form-item>
     </el-form>
   </el-row>
+
 </template>
 
 <script>
   export default {
-    name: 'Login'
+    name: 'Login',
+    data() {
+      var validateName = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入用户名'));
+        } else {
+          callback();
+        }
+      };
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.ruleForm.checkPass !== '') {
+            this.$refs.ruleForm.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.ruleForm.pass) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
+      return {
+        isLogin: true,
+        ruleForm: {
+          name: '',
+          pass: '',
+          checkPass: '',
+        },
+        rules: {
+        /*  trigger: ‘blur’ 表示“当失去焦点时（光标不显示的时候），触发此提示”
+           此处应该是有一个校验，若失去焦点，则触发trigger进行校验，若校验不成功，则进行提示*/
+          name: [
+            { validator: validateName, trigger: 'blur' }
+          ],
+          pass: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          checkPass: [
+            { validator: validatePass2, trigger: 'blur' }
+          ],
+        }
+      };
+    },
+    methods: {
+      login() {
+        this.$router.push('/home')
+      },
+      reg() {
+        this.isLogin = false
+      },
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+
   }
 </script>
 
